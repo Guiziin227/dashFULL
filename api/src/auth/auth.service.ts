d3712ca48ac7890eee8fdbd6f5eb17bc06398c27
Guiziin227 <gh63388@gmail.com>
2025-10-01 11:28:15 -0300

    Service de validacao

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,19 +1,38 @@
-import {ConflictException, Injectable} from '@nestjs/common';
-import {CreateUserDto} from "../user/dto/create-user.dto";
-import {UserService} from "../user/user.service";
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import { UserService } from '../user/user.service';
+import { verify } from 'argon2';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly userService: UserService) {}
 
-    constructor(private readonly userService: UserService) {}
+  async registerUser(createUserDto: CreateUserDto) {
+    const user = await this.userService.findByEmail(createUserDto.email);
 
-    async registerUser(createUserDto: CreateUserDto) {
-        const user = await this.userService.findByEmail(createUserDto.email);
+    if (user) {
+      throw new ConflictException('User already exists');
+    }
+    return this.userService.create(createUserDto);
+  }
 
-        if (user) {
-            throw new ConflictException('User already exists');
-        }
-        return this.userService.create(createUserDto);
+  async validateLocalUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException('User not found');
     }
 
+    const isPasswordValid = verify(user.password, password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Credentials are not valid');
+    }
+
+    return { id: user.id, name: user.name };
+  }
 }
