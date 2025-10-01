6930cf11963242b209d4169a5909609fb53369c3
Guiziin227 <gh63388@gmail.com>
2025-09-30 21:54:37 -0300

    Implementa validação e integração da funcionalidade de cadastro

--- web/src/app/auth/signup/singupForm.tsx
+++ web/src/app/auth/signup/singupForm.tsx
@@ -1,28 +1,58 @@
+"use client"
+
 import React from 'react';
 import {Label} from "@/components/ui/label";
 import {Input} from "@/components/ui/input";
 import SubmitButton from "@/components/ui/submitButton";
+import { useFormState } from "react-dom";
+import { signUp } from "@/lib/auth";
 
 const SingupForm = () => {
+
+    const [state,action] = useFormState(signUp,undefined);
+
     return (
-        <form>
+        <form action={action} onSubmit={SingupForm}>
             <div className="flex flex-col gap-4">
+              {state?.message && (
+                <div className="text-red-500 text-sm">{state.message}</div>
+              )}
+
                 <div>
                     <Label htmlFor="name">Name</Label>
                     <Input id="name" name="name" type="text" placeholder="John Doe"/>
                 </div>
+              {state?.error?.name && (
+                <div className="text-red-500 text-sm">{state.error.name}</div>
+              )}
 
                 <div>
                     <Label htmlFor="email">Email</Label>
                     <Input id="email" name="email" type="text" placeholder="Johndoe@email.com"/>
                 </div>
 
+              {state?.error?.email && (
+                <div className="text-red-500 text-sm">{state.error.email}</div>
+              )}
+
                 <div>
                     <Label htmlFor="password">Password</Label>
                     <Input id="password" name="password" type="password" />
                 </div>
 
-                <SubmitButton>Submit</SubmitButton>
+              {state?.error?.password && (
+                <div className="text-red-500 text-sm">
+                  <p>Password must:</p>
+                  <ul>
+                    {state.error.password.map((err, index) => (
+                      <li key={index}>- {err}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+
+
+                <SubmitButton>Sign Up</SubmitButton>
             </div>
         </form>
     );
--- web/src/lib/type.ts
+++ web/src/lib/type.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export type FormState = {
     error?: {
-        name?: string;
-        email?: string;
+        name?: string[];
+        email?: string[];
         password?: string[];
     };
     message?: string;
