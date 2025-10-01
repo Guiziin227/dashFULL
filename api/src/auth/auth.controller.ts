ee0bbcec2e9d16d9210bf36dee832ab21786c351
Guiziin227 <gh63388@gmail.com>
2025-10-01 11:28:33 -0300

    controller

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import {CreateUserDto} from "../user/dto/create-user.dto";
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import { LocalAuthGuard } from './guards/local-auth/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-    @Post('signup')
-    registerUser(@Body() createUserDto: CreateUserDto) {
-        return this.authService.registerUser(createUserDto);
-    }
+  @Post('signup')
+  registerUser(@Body() createUserDto: CreateUserDto) {
+    return this.authService.registerUser(createUserDto);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('signin')
+  loginUser(@Request() req) {
+    return req.user;
+  }
 }
